79438f9b70d01088a6b30ca99c902ce2e855ebbe
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-03-05 08:16:55 -1000

    first commit

---
+++ app/src/main/java/com/myappcompany/russell/photoimportdemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.myappcompany.russell.photoimportdemo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri selectedImage = data.getData();
+
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null){
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
